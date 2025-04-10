6c3893270728812dfec499dddcdea2937a7af9c5
nmktad <nmktadesse@gmail.com>
2025-04-11 00:48:48 +0300

    feat: add installablility. need some improvements and bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,107 +1,26 @@
-"use client";
-
-import { useState, useEffect } from "react";
+import InstallButton from "@/components/install-drawer";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-function InstallButton() {
-  const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null);
-  const [isInstallable, setIsInstallable] = useState(false);
-
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (e: Event) => {
-      e.preventDefault();
-      setDeferredPrompt(e);
-      setIsInstallable(true);
-    };
-
-    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
-
-    return () =>
-      window.removeEventListener(
-        "beforeinstallprompt",
-        handleBeforeInstallPrompt,
-      );
-  }, []);
-
-  const handleInstallClick = async () => {
-    if (deferredPrompt) {
-      (deferredPrompt as any).prompt();
-      const { outcome } = await (deferredPrompt as any).userChoice;
-      setDeferredPrompt(null);
-      setIsInstallable(false);
-      console.log(`User response to the install prompt: ${outcome}`);
-    }
-  };
-
-  return (
-    <>
-      {isInstallable && (
-        <Button onClick={handleInstallClick}>Install App</Button>
-      )}
-    </>
-  );
-}
-
-function InstallPrompt() {
-  const [isIOS, setIsIOS] = useState(false);
-  const [isStandalone, setIsStandalone] = useState(false);
-
-  useEffect(() => {
-    setIsIOS(
-      /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream,
-    );
-
-    setIsStandalone(window.matchMedia("(display-mode: standalone)").matches);
-  }, []);
-
-  if (isStandalone) {
-    return null;
-  }
-
-  return (
-    <div>
-      <InstallButton />
-      {isIOS && (
-        <p>
-          To install this app on your iOS device, tap the share button
-          <span role="img" aria-label="share icon">
-            {" "}
-            ⎋{" "}
-          </span>
-          and then "Add to Home Screen"
-          <span role="img" aria-label="plus icon">
-            {" "}
-            ➕{" "}
-          </span>
-          .
-        </p>
-      )}
-    </div>
-  );
-}
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="grid font-mono grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <InstallPrompt />
+        <InstallButton />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <div className="p-4 overflow-hidden w-14 h-14 hover:w-full hover:max-w-xl hover:place-items-start bg-primary rounded-full flex group items-center hover:duration-300 duration-300">
-          <div className="flex items-center justify-center fill-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className="w-6 h-6"
-            >
-              <path d="M18.9,16.776A10.539,10.539,0,1,0,16.776,18.9l5.1,5.1L24,21.88ZM10.5,18A7.5,7.5,0,1,1,18,10.5,7.507,7.507,0,0,1,10.5,18Z"></path>
-            </svg>
+      <footer className="row-start-3 flex gap-[24px] w-full flex-wrap items-center justify-center">
+        <form className="grid gap-4">
+          <div className="grid gap-2">
+            <div className="flex gap-2">
+              <Input
+                id="search-word"
+                placeholder="Enter a word to search"
+                className="flex-1 min-w-xs"
+              />
+              <Button type="submit">Search</Button>
+            </div>
           </div>
-          <input
-            type="text"
-            className="outline-none text-lg bg-transparent w-full text-white font-normal px-4"
-          />
-        </div>
+        </form>
       </footer>
     </div>
   );
---
+++ src/components/install-drawer.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+export default function InstallButton() {
+  const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null);
+  const [isInstallable, setIsInstallable] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: Event) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+      setIsInstallable(true);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () =>
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt,
+      );
+  }, []);
+
+  const handleInstallClick = async () => {
+    if (deferredPrompt) {
+      (deferredPrompt as any).prompt();
+      const { outcome } = await (deferredPrompt as any).userChoice;
+      setDeferredPrompt(null);
+      setIsInstallable(false);
+      console.log(`User response to the install prompt: ${outcome}`);
+    }
+  };
+
+  return (
+    <>
+      {isInstallable && (
+        <InstallDrawer
+          handler={handleInstallClick}
+          installable={isInstallable}
+        />
+      )}
+    </>
+  );
+}
+
+function InstallDrawer({
+  handler,
+  installable,
+}: {
+  handler: () => Promise<void>;
+  installable: boolean;
+}) {
+  if (!installable) return null;
+
+  const [defaultOpen, setDefaultOpen] = useState(true);
+  const [disabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    setDefaultOpen(
+      localStorage.getItem("install-drawer-shown") != "displayed" &&
+        !window.matchMedia("(display-mode: standalone)").matches,
+    );
+    setDisabled(
+      typeof window === "undefined" ||
+        window.matchMedia("(display-mode: standalone)").matches,
+    );
+  }, []);
+
+  return (
+    <Drawer defaultOpen={defaultOpen && installable} open={installable}>
+      <DrawerTrigger asChild>
+        <Button variant="outline" disabled={disabled && !installable}>
+          Open Drawer
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Install the app</DrawerTitle>
+            <DrawerDescription></DrawerDescription>
+          </DrawerHeader>
+
+          {/*checks if it's ios device*/}
+          {/iPad|iPhone|iPod/.test(navigator.userAgent) &&
+          !(window as any).MSStream ? (
+            <p>
+              To install this app on your iOS device, tap the share button
+              <span role="img" aria-label="share icon">
+                {" "}
+                ⎋{" "}
+              </span>
+              and then "Add to Home Screen"
+              <span role="img" aria-label="plus icon">
+                {" "}
+                ➕{" "}
+              </span>
+              .
+            </p>
+          ) : (
+            <div>
+              <p>This lets you get the best experience out of the platform</p>
+
+              <Button
+                onClick={() => {
+                  handler();
+                }}
+              >
+                Install App
+              </Button>
+              <DrawerClose asChild>
+                <Button
+                  variant="outline"
+                  onClick={() => {
+                    localStorage.setItem("install-drawer-shown", "displayed");
+                  }}
+                >
+                  Cancel
+                </Button>
+              </DrawerClose>
+            </div>
+          )}
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
